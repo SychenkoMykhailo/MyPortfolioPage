53e3c9a276c14090b79b6d8fdb1b91f3b429bd16
Инна Головко <InnaInnaYu@gmail.com>
2022-06-05 16:41:04 +0300

    delete header

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,6 @@ import Header from "../views/Header";
 function App() {
   return (
     <div className="wrapper">
-      <h1>Test</h1>
       <Header />
       <Navigation />
       <div className="container">
